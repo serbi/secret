eab0c22d2a4d60d08d2769ec2dafaee55fe5af48
Damian Sygidus <syg.dam@gmail.com>
2019-10-03 14:39:18 +0200

    File-backed version

--- cmd/demo.go
+++ cmd/demo.go
@@ -6,16 +6,38 @@ import (
 )
 
 func main() {
-	v := secret.Memory("my-fake-key")
-	err := v.Set("demo_key", "some random value")
+	v := secret.File("my-fake-key", ".secrets")
+
+	err := v.Set("demo_key_1", "some random value 1")
 	if err != nil {
 		panic(err)
 	}
 
-	plain, err := v.Get("demo_key")
+	err = v.Set("demo_key_2", "some random value 2")
 	if err != nil {
 		panic(err)
 	}
 
+	err = v.Set("demo_key_3", "some random value 3")
+	if err != nil {
+		panic(err)
+	}
+
+	plain, err := v.Get("demo_key_1")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Plain:", plain)
+
+	plain, err = v.Get("demo_key_2")
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Plain:", plain)
+
+	plain, err = v.Get("demo_key_3")
+	if err != nil {
+		panic(err)
+	}
 	fmt.Println("Plain:", plain)
 }
